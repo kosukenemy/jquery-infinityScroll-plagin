51a54088ee083fa4530c1e0e5bd8a3f90492638b
kouskenemy <kosukenemy@gmail.com>
2021-10-02 23:06:10 +0900

    first push

---
+++ README.md
@@ -0,0 +1,64 @@
+
+# JQuery Infinity Scroll (plagin)
+
+### How it works
+This plugin is a JQuery plugin that enables infinite scrolling.
+Download `infinityScroll.css` and `infinityScroll.min.js` in the `dist directory` of this repository and load them into your HTML file.
+
+- ja
+このプラグインは無限スクロールを実現するJQueryプラグインです。
+このリポジトリの`distディレクトリ`にある`infinityScroll.css`と`infinityScroll.min.js`をダウンロードして、HTMLファイルに読み込んでください。
+
+```HTML
+<!-- read <head> infinityScroll.css -->
+<link rel="stylesheet" href="dist/infinityScroll.css">
+<!-- read <body> infinityScroll.min.js -->
+<script src="dist/infinityScroll.min.js"></script>
+```
+
+
+### Infinity Scroll Init / Set Up
+
+After loading `infinityScroll.min.js` into an HTML file,
+set up the `<script>` tag with the following code.
+Display a message after loading all content.
+
+- ja
+`infinityScroll.min.js`をHTMLファイルに読み込んだ後に、
+`<script>`タグに以下のようなコードでセットアップをおこないます。
+Ajaxで読み込むHTMLはload.htmlに記述します。
+
+- config
+
+```JavaScript
+const config = {
+        type : "html", 
+        data : "load.html", // infinite scrolling Contents HTML
+        loadListClass : "is-load-list", // infinite scrolling Contents HTML ClassList
+        renderInit : 2, //　initial value　:2　　 It works even if false.　 List to display first
+        scrollEndMessage : "read all contents!", // It works even if false. Display a message after loading all content.
+    }
+
+// infinite scrolling Contents Show ParentNode
+$('#element').infinityScroll(config)
+```
+
+- load.html
+````HTML
+
+<!-- config.data  -->
+<div class="is-load-list"> <!-- config.loadListClass's ClassList -->
+    <img src="smaple/images/a5cJ6tTI8uQ.jpg" alt="">
+</div>
+<div class="is-load-list">
+    <img src="smaple/images/BKxFj0ogp2M.jpg" alt="">
+</div>
+<div class="is-load-list">
+    <img src="smaple/images/FU3vZToCZQ4.jpg" alt="">
+</div>
+````
+
+### license
+MIT License
+
+
---
+++ src/index.js
@@ -0,0 +1,85 @@
+
+/* jQuery infinityScroll plagin by kosukenemy */
+
+(($) => {
+    $.fn.infinityScroll = function(setting) {
+        const _this = this;
+        setting.data == null && 'undefined' ? null : get_ajax_settings();
+
+        function get_ajax_settings(){
+            const params = {
+                type: 'GET',
+                url: setting.data,
+                dataType: setting.type,
+            }
+            $.when( $.ajax( params ) )
+            .done( function(data) { ajaxdata_loadComplate(data) })
+            .fail( function(err) {
+                console.error(err)
+            })
+            // console.log('ajax await...')
+        }
+
+
+        function ajaxdata_loadComplate(data){
+            $(data).length === 0 ?
+            console.error(` error: option ${data} is not defind `) || null
+            : 
+            documentScroll(data);
+        }
+
+        function scroll_countUp(jQueryObject , init) {
+            return  _this.append( $(jQueryObject).slice(0 , init * 2 ));
+        }
+
+        function documentScroll(data , render){
+            setting.renderInit == null ? settedInit() : settedAddOpt();
+            
+
+            function settedInit(){
+                const init = 2;
+                scroll_countUp(data ,init) 
+                return render = init;
+            }
+
+            function settedAddOpt(){
+                scroll_countUp(data ,setting.renderInit);
+                return render = setting.renderInit;
+            }
+
+
+            
+            $(window).on('load scroll resize' , function() {
+                const docHight = $(document).innerHeight();
+                const windowHight = $(window).innerHeight();
+                const pageBottom = docHight - windowHight;
+                const list = $("." + setting.loadListClass).not('text');
+                const percent = 0.95;   
+
+
+                if ( pageBottom * percent <= $(window).scrollTop() ){
+                    
+                    $.when ( _this.html("") )
+                    .done (
+                        $("." + setting.loadListClass).fadeIn(300),
+                        scroll_countUp(data ,list.length + 1)
+                    )
+                    $("." + setting.loadListClass).last().addClass('is-on')
+                    
+                    if ( setting.scrollEndMessage !== -1 && 
+                        $(data).not('text').length === list.length ) 
+                    {
+                        $('<div>' , { id: "end-message", text: setting.scrollEndMessage })
+                        .appendTo( _this );
+                        $("." + setting.loadListClass).removeClass('is-on');
+                    } else {
+                        return null
+                    }
+
+                }
+            })
+        }
+
+    
+    };
+})(jQuery);
